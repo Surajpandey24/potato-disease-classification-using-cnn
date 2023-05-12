95f42432accbc8ab5d6f091e5e57a00016b1d008
Suraj Pandey <pandeysuraj732@gmail.com>
2023-05-12 14:22:53 +0530

    api setup

---
+++ app.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+from fastapi import FastAPI, File, UploadFile
+import uvicorn 
+
+app = FastAPI()
+
+Model= tf.keras.models.load_model("../models/1")
+CLASS_NAMES=["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am Suraj"
+
+def read_file_as_image(data)-> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
+@app.post("/predict")
+async def predict(
+    file: UploadFile
+):
+    image=read_file_as_image(await file.read())
+    image_batch=np.expand_dims(image,0)
+    predictions=Model.predict(image_batch)
+    predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
+    confidence= np.max(predictions[0])
+
+    return {
+        'class': predicted_class,
+        'confidence':float(confidence)
+    }
+
+if __name__=="__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
+
